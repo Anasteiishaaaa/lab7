fcc2c7ad98d2f3fe9390918b016ef0ddbd2560e9
anastasiia <nastya.kovaleva2307@gmail.com>
2026-04-18 22:54:24 +0300

    Implement d (index-based modification (insert_at, remove_at)), h (output via cout)

--- lab7/SinglyLinkedList.h
+++ lab7/SinglyLinkedList.h
@@ -78,6 +78,40 @@ public:
 		return current->data;
 	}
 
+	void insert_at(size_t index, const T& val) {
+		if (index > length) {
+			throw std::out_of_range("Index out of range");
+		}
+		if (index == 0) {
+			push_front(val);
+			return;
+		}
+		auto newNode = std::make_shared<Node<T>>(val);
+		auto current = head;
+		for (size_t i = 0; i < index - 1; i++) {
+			current = current->next;
+		}
+		newNode->next = current->next;
+		current->next = newNode;
+		length++;
+	}
+
+	void remove_at(size_t index) {
+		if (index >= length) {
+			throw std::out_of_range("Index out of range");
+		}
+		if (index == 0) {
+			pop_front();
+			return;
+		}
+		auto current = head;
+		for (size_t i = 0; i < index - 1; i++) {
+			current = current->next;
+		}
+		current->next = current->next->next;
+		length--;
+	}
+
 	int find(const T& value) const {
 		auto current = head;
 		int index = 0;
@@ -89,5 +123,15 @@ public:
 			index++;
 		}
 		return -1;
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, const SinglyLinkedList<T>& list) {
+		auto current = list.head;
+		while (current) {
+			os << current->data << " -> ";
+			current = current->next;
+		}
+		os << "nullptr";
+		return os;
 	}
 };
