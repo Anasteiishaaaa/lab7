e68e544750b17b704f95856ba16b160385511afb
anastasiia <nastya.kovaleva2307@gmail.com>
2026-04-18 22:43:20 +0300

    Add a (push_front / push_back), e (size), f (isEmpty) methods

--- lab7/SinglyLinkedList.h
+++ lab7/SinglyLinkedList.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "Node.h"
 #include <stdexcept>
+
 template <typename T>
 class SinglyLinkedList {
 private:
@@ -9,4 +10,34 @@ private:
 	size_t length;
 public:
 	SinglyLinkedList() : head(nullptr), length(0) {}
+
+	bool isEmpty() const {
+		return length == 0;
+	}
+
+	size_t size() const {
+		return length;
+	}
+
+	void push_front(const T& value) {
+		auto newNode = std::make_shared<Node<T>>(value);
+		newNode->next = head;
+		head = newNode;
+		length++;
+	}
+
+	void push_back(const T& value) {
+		auto newNode = std::make_shared<Node<T>>(value);
+		if (isEmpty()) {
+			head = newNode;
+		}
+		else {
+			auto current = head;
+			while (current->next) {
+				current = current->next;
+			}
+			current->next = newNode;
+		}
+		length++;
+	}
 };
